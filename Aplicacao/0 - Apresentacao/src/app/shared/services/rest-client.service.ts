f0d093adf28ab854a42caa1a6fa9bcb0b660ac78
Lennon Vinicius Alves Dias <lennon.dias@connectparts.com.br>
2018-06-18 23:22:11 -0300

    fix 401 rest client

--- Aplicacao/0 - Apresentacao/src/app/shared/services/rest-client.service.ts
+++ Aplicacao/0 - Apresentacao/src/app/shared/services/rest-client.service.ts
@@ -1,11 +1,15 @@
-import { map } from 'rxjs/operators';
 import { Http, Response, Headers } from '@angular/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { CookieService } from 'ng2-cookies';
 
 import { ApiConfig } from '../models/api-config.interface';
 import { API_BASE_URL } from '../shared.constants';
+
+import { map, catchError } from 'rxjs/operators';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/throw';
 
 @Injectable({
   providedIn: 'root'
@@ -18,6 +22,7 @@ export class RestClientService {
   constructor(
     private _http: Http,
     private _cookieService: CookieService,
+    private _rota: Router
   ) {
     this._headers.set('Content-Type', 'application/json');
     this._baseUrl = API_BASE_URL;
@@ -54,34 +59,59 @@ export class RestClientService {
   get(config: ApiConfig, segmento: string = null, params = null, headers: Map<string, string> = null, autentication: boolean = true) {
     const url = this.montarRequisicao(config, segmento, headers, autentication);
     return this._http.get(url, { headers: this._headers, params: params }).pipe(
-      map((res: Response) => res.json()));
+      map((res: Response) => res.json()),
+      catchError(error => {
+        if (error.status === 401) { this.unauthorized(); }
+        return Observable.throw(error);
+      })
+    );
   }
 
   // tslint:disable-next-line:max-line-length
   post(config: ApiConfig, segmento: string = null, body: any, params = null, headers: Map<string, string> = null, autentication: boolean = true) {
     const url = this.montarRequisicao(config, segmento, headers, autentication);
     return this._http.post(url, JSON.stringify(body), { headers: this._headers, params: params }).pipe(
-      map((res: Response) => res.json()));
+      map((res: Response) => res.json()),
+      catchError((err) => {
+        if (err.status === 401) { this.unauthorized(); }
+        return Observable.throw(err);
+      })
+    );
   }
 
   // tslint:disable-next-line:max-line-length
   put(config: ApiConfig, segmento: string = null, body: any, params = null, headers: Map<string, string> = null, autentication: boolean = true) {
     const url = this.montarRequisicao(config, segmento, headers, autentication);
     return this._http.put(url, JSON.stringify(body), { headers: this._headers, params: params }).pipe(
-      map((res: Response) => res.json()));
+      map((res: Response) => res.json()),
+      catchError((err) => {
+        if (err.status === 401) { this.unauthorized(); }
+        return Observable.throw(err);
+      })
+    );
   }
 
   // tslint:disable-next-line:max-line-length
   patch(config: ApiConfig, segmento: string = null, body: any, params = null, headers: Map<string, string> = null, autentication: boolean = true) {
     const url = this.montarRequisicao(config, segmento, headers, autentication);
     return this._http.patch(url, JSON.stringify(body), { headers: this._headers, params: params }).pipe(
-      map((res: Response) => res.json()));
+      map((res: Response) => res.json()),
+      catchError((err) => {
+        if (err.status === 401) { this.unauthorized(); }
+        return Observable.throw(err);
+      })
+    );
   }
 
   delete(config: ApiConfig, segmento: string = null, params = null, headers: Map<string, string> = null, autentication: boolean = true) {
     const url = this.montarRequisicao(config, segmento, headers, autentication);
     return this._http.delete(url, { headers: this._headers, params: params }).pipe(
-      map((res: Response) => res.text() ? res.json() : {}));
+      map((res: Response) => res.json()),
+      catchError((err) => {
+        if (err.status === 401) { this.unauthorized(); }
+        return Observable.throw(err);
+      })
+    );
   }
 
   openUrl(config: ApiConfig, segmento: string = null, params = null, headers: Map<string, string> = null, autentication: boolean = true) {
@@ -91,6 +121,11 @@ export class RestClientService {
 
   private get autenticacao() {
     return `Bearer ${this._cookieService.get('accessToken')}`;
+  }
+
+  private unauthorized() {
+    this._cookieService.deleteAll();
+    this._rota.navigate(['/login']);
   }
 
 }
